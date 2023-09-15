8c4a087e2a4b49d049be7c4ea18259d4fc4db201
Cedric Rossi <cedric@terramater.net>
2023-09-15 22:34:03 +0200

    Habillage gouv.fr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,86 @@
 <script>
   import "../app.postcss";
+  import LogoRepublique from "$lib/assets/logos/logo-rf.svg";
 </script>
 
-<main class="m-4">
+<header class="mx-8 my-4 flex flex-row gap-12">
+  <img
+    class="mr-s40"
+    src={LogoRepublique}
+    alt="République française - Liberté, Égalité, Fraternité"
+    width="124"
+    height="110"
+  />
+  <div class="flex items-center">
+    <div class="flex flex-col">
+      <div class="text-3xl font-bold">Simulateur territoires</div>
+      <div class="text-xl text-marianne-red">
+        Version Beta — ne pas diffuser
+      </div>
+    </div>
+  </div>
+</header>
+
+<hr />
+
+<main class="mx-8 my-4">
   <slot />
 </main>
+
+<hr class="my-8 border-t-2 border-title-blue-france" />
+
+<footer class="mx-8 mb-8">
+  <div class="flex flex-row gap-8">
+    <div class="mb-8 w-1/2">
+      <img
+        class="inline"
+        src={LogoRepublique}
+        alt="République Française - Liberté, Égalité, Fraternité"
+        width="124"
+        height="110"
+      />
+    </div>
+    <div class="w-1/2 text-base leading-normal">
+      <div class="mt-4 flex flex-wrap gap-x-8 gap-y-2 font-bold">
+        <a
+          target="_blank"
+          title="Ouverture dans une nouvelle fenêtre"
+          rel="noopener"
+          href="https://gouvernement.fr">gouvernement.fr</a
+        >
+        <a
+          target="_blank"
+          title="Ouverture dans une nouvelle fenêtre"
+          rel="noopener"
+          href="https://service-public.fr">service-public.fr</a
+        >
+        <a
+          target="_blank"
+          title="Ouverture dans une nouvelle fenêtre"
+          rel="noopener"
+          href="https://data.gouv.fr">data.gouv.fr</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <hr class="mb-4" />
+
+  <nav class="flex flex-wrap gap-4 text-sm text-gray-400">
+    <a class="block hover:underline" href="/mentions-legales"
+      >Mentions légales</a
+    >
+    <a class="block hover:underline" href="/politique-de-confidentialite"
+      >Données personnelles</a
+    >
+    <a class="block hover:underline" href="/accessibilite"
+      >Accessibilité : non conforme</a
+    >
+  </nav>
+</footer>
+
+<style lang="postcss">
+  a {
+    @apply block hover:underline;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,8 @@
   import Slide4 from "./slide4.svelte";
 </script>
 
-<h1 class="text-3xl font-bold text-title-blue-france">
-  Simulateur territoires
-</h1>
+<svelte:head>
+  <title>Simulateur territoires</title>
+</svelte:head>
 
-<strong class="text-xl text-marianne-red">Version Beta — Ne pas diffuser</strong
->
-<div class="m-4">
-  <Slide4></Slide4>
-</div>
+<Slide4></Slide4>
