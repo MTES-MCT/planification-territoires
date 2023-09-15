169f341bb2ae0631dad9a3eeba1a6f2ab1a81040
Cedric Rossi <cedric@terramater.net>
2023-09-15 15:25:09 +0200

    Ajout de `frame-ancestors 'none'` à la CSP

--- svelte.config.js
+++ svelte.config.js
@@ -24,6 +24,7 @@ const config = {
         "object-src": ["none"],
         "script-src": ["self", "strict-dynamic", "unsafe-inline"],
         "style-src": ["self", "unsafe-inline"],
+        "frame-ancestors": ["none"],
       },
     },
   },
