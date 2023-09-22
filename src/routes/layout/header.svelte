d2e824653da9b2f687badfa459aaaea925bd803b
Cédric Soulas <soulascedric@gmail.com>
2023-09-22 11:43:02 +0200

    Ajout d'un header DSFR

--- src/routes/layout/header.svelte
+++ src/routes/layout/header.svelte
@@ -1,22 +1,34 @@
-<script lang="ts">
-  import LogoRepublique from "$lib/assets/logos/logo-rf.svg";
-</script>
-
-<header class="mx-8 my-4 flex flex-row gap-12">
-  <img
-    class="mr-s40"
-    src={LogoRepublique}
-    alt="République française - Liberté, Égalité, Fraternité"
-    width="124"
-    height="110"
-  />
-  <div class="flex items-center">
-    <div class="flex flex-col">
-      <a href="/" class="block text-3xl font-bold !no-underline"
-        >Simulateur territoires</a
-      >
-      <div class="text-xl text-marianne-red">
-        Version Beta — ne pas diffuser
+<header class="fr-header">
+  <div class="fr-header__body">
+    <div class="fr-container">
+      <div class="fr-header__body-row">
+        <div class="fr-header__brand fr-enlarge-link">
+          <div class="fr-header__brand-top">
+            <div class="fr-header__logo">
+              <p class="fr-logo">
+                République
+                <br />Française
+              </p>
+            </div>
+          </div>
+          <div class="fr-header__service">
+            <a
+              href="/"
+              title="Accueil - Simulateur territoires - Secrétariat général à la planification écologique"
+            >
+              <p class="fr-header__service-title">Simulateur territorial</p>
+            </a>
+            <p class="fr-header__service-tagline">
+              Secrétariat général à la planification écologique - Service de la
+              Première ministre
+            </p>
+          </div>
+        </div>
+        <div
+          class="fr-header__tools text-sm font-semibold uppercase text-marianne-red"
+        >
+          Version Beta - Ne pas diffuser
+        </div>
       </div>
     </div>
   </div>
