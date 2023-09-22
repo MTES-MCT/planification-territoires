3db255a5f7dcce0e68c03afe59fed808589b1824
Cédric Soulas <soulascedric@gmail.com>
2023-09-22 11:43:41 +0200

    Ajout d'un footer DSFR

--- src/routes/layout/footer.svelte
+++ src/routes/layout/footer.svelte
@@ -1,63 +1,75 @@
-<script lang="ts">
-  import LogoRepublique from "$lib/assets/logos/logo-rf.svg";
-</script>
-
-<hr class="my-8 border-t-2 border-title-blue-france" />
-
-<footer class="mx-8 mb-8">
-  <div class="flex flex-row gap-8">
-    <div class="mb-8 w-1/2">
-      <img
-        class="inline"
-        src={LogoRepublique}
-        alt="République Française - Liberté, Égalité, Fraternité"
-        width="124"
-        height="110"
-      />
+<footer class="fr-footer" id="footer">
+  <div class="fr-container">
+    <div class="fr-footer__body">
+      <div class="fr-footer__brand fr-enlarge-link">
+        <a
+          href="/"
+          title="Retour à l’accueil du site - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)"
+        >
+          <p class="fr-logo">République<br />Française</p></a
+        >
+      </div>
+      <div class="fr-footer__content">
+        <p class="fr-footer__content-desc">
+          Un outil de simulation des objectifs territoriaux
+        </p>
+        <ul class="fr-footer__content-list">
+          <li class="fr-footer__content-item">
+            <a
+              class="fr-footer__content-link"
+              target="_blank"
+              href="https://legifrance.gouv.fr">legifrance.gouv.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              class="fr-footer__content-link"
+              target="_blank"
+              href="https://gouvernement.fr">gouvernement.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              class="fr-footer__content-link"
+              target="_blank"
+              href="https://service-public.fr">service-public.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              class="fr-footer__content-link"
+              target="_blank"
+              href="https://data.gouv.fr">data.gouv.fr</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="w-1/2 leading-normal">
-      <div class="mt-4 flex flex-wrap gap-x-8 gap-y-2 text-sm font-bold">
-        <a
-          target="_blank"
-          title="Ouverture dans une nouvelle fenêtre"
-          rel="noopener"
-          href="https://gouvernement.fr">gouvernement.fr</a
-        >
-        <a
-          target="_blank"
-          title="Ouverture dans une nouvelle fenêtre"
-          rel="noopener"
-          href="https://service-public.fr">service-public.fr</a
-        >
-        <a
-          target="_blank"
-          title="Ouverture dans une nouvelle fenêtre"
-          rel="noopener"
-          href="https://data.gouv.fr">data.gouv.fr</a
-        >
+    <div class="fr-footer__bottom">
+      <ul class="fr-footer__bottom-list">
+        <li class="fr-footer__bottom-item">
+          <span class="fr-footer__bottom-link"
+            >Accessibilité : non conforme</span
+          >
+        </li>
+        <li class="fr-footer__bottom-item">
+          <span class="fr-footer__bottom-link">Mentions légales</span>
+        </li>
+        <li class="fr-footer__bottom-item">
+          <span class="fr-footer__bottom-link">Données personnelles</span>
+        </li>
+      </ul>
+      <div class="fr-footer__bottom-copy">
+        <p>
+          Sauf mention explicite de propriété intellectuelle détenue par des
+          tiers, les contenus de ce site sont proposés sous
+          <a
+            href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+            target="_blank"
+            >licence etalab-2.0
+          </a>
+        </p>
       </div>
     </div>
   </div>
-
-  <hr class="mb-4" />
-
-  <nav class="flex flex-wrap gap-4 text-xs text-gray-500">
-    <a class="block hover:underline" href="/mentions-legales"
-      >Mentions légales</a
-    >
-    |
-    <a class="block hover:underline" href="/politique-de-confidentialite"
-      >Données personnelles</a
-    >
-    |
-    <a class="block hover:underline" href="/accessibilite"
-      >Accessibilité : non conforme</a
-    >
-  </nav>
 </footer>
-
-<style lang="postcss">
-  a {
-    @apply block hover:underline;
-  }
-</style>
