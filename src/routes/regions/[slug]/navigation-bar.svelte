c638d2b73cbdbd9ebe60243c3a86e318d5c22e62
Cédric Soulas <soulascedric@gmail.com>
2023-09-22 19:56:01 +0200

    Fix choix de la région sur mobile

--- src/routes/regions/[slug]/navigation-bar.svelte
+++ src/routes/regions/[slug]/navigation-bar.svelte
@@ -3,7 +3,7 @@
   export let title;
 </script>
 
-<div class="mb-2 flex w-full items-end justify-between">
+<div class="mb-2 flex w-full flex-wrap items-end justify-between">
   <h1 class="mb-0 mt-4">{title}</h1>
   <div>
     <div class="mb-0">{region}</div>
