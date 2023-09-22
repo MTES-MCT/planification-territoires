1f03450f8400f424ea4596d39763ab2d215c27d1
Cedric Rossi <cedric@terramater.net>
2023-09-22 12:52:46 +0200

    Sauvegarde du state dans l’URL

--- src/routes/regions/[slug]/objectifs/+page.svelte
+++ src/routes/regions/[slug]/objectifs/+page.svelte
@@ -1,41 +1,43 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import { goto } from "$app/navigation";
-
-  import { tidy, filter } from "@tidyjs/tidy";
 
   import MainTreemap from "$lib/main-treemap.svelte";
   import CompletionLevelInput from "./completion-level-input.svelte";
 
-  import type { CompletionLevels, Lever } from "$lib/types";
-
   export let data;
-  import rawLeversData from "$lib/data.json";
-  const leversData = rawLeversData as Lever[];
 
-  const regionData: Lever[] = tidy(
-    leversData,
-    filter((d) => d.region === data.region)
-  );
-
-  let completionLevels: CompletionLevels = Object.fromEntries(
-    regionData.map((l) => [l.name, 0])
-  );
-
-  console.log(data);
+  // À chaque modification de data.completionLevels, on met à jour
+  // la querystring
+  $: {
+    Object.entries(data.completionLevels).forEach((c) =>
+      $page.url.searchParams.set(c[0], c[1])
+    );
+    goto(`?${$page.url.searchParams.toString()}`, {
+      keepFocus: true,
+      noScroll: true,
+    });
+  }
 </script>
 
 <svelte:head>
   <title>{data.region} | Objectifs | Simulateur territoires</title>
 </svelte:head>
 
-{#each regionData as lever}
+{#each data.regionData as lever}
   <div class="my-4 rounded border bg-gray-50 p-4">
-    <CompletionLevelInput {lever} bind:completionLevels />
+    <CompletionLevelInput
+      {lever}
+      bind:completionLevels={data.completionLevels}
+    />
   </div>
 {/each}
 
 <div class="h-96">
-  <MainTreemap data={regionData} {completionLevels} />
+  <MainTreemap
+    data={data.regionData}
+    completionLevels={data.completionLevels}
+  />
 </div>
 
 <button
--- src/routes/regions/[slug]/objectifs/+page.ts
+++ src/routes/regions/[slug]/objectifs/+page.ts
@@ -1,7 +1,29 @@
 import type { PageLoad } from "./$types";
+import type { CompletionLevels, Lever } from "$lib/types";
 
-export const load: PageLoad = async ({ params }) => {
+import { tidy, filter } from "@tidyjs/tidy";
+
+import rawLeversData from "$lib/data.json";
+const leversData = rawLeversData as Lever[];
+
+export const load: PageLoad = async ({ params, url }) => {
+  const regionData: Lever[] = tidy(
+    leversData,
+    filter((d) => d.region === params.slug)
+  );
+
+  const completionLevels: CompletionLevels = Object.fromEntries(
+    regionData.map((l) => [l.name, 0])
+  );
+
+  // Mise à jour de completionsLevels avec les paramètres de l’URL
+  url.searchParams.forEach((value, key) => {
+    completionLevels[key] = Number(value);
+  });
+
   return {
     region: params.slug,
+    regionData,
+    completionLevels,
   };
 };
