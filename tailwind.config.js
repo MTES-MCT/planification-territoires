18bebfd31342f35e083f5d58147123ec0ffaf882
Cedric Rossi <cedric@terramater.net>
2023-09-23 19:47:23 +0200

    Utilisation des breakpoints du DSFR

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
+    screens: {
+      // https://www.systeme-de-design.gouv.fr/elements-d-interface/fondamentaux-techniques/grille-et-points-de-rupture/
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1248px",
+    },
     extend: {
       colors: {
         // https://www.systeme-de-design.gouv.fr/elements-d-interface/fondamentaux-de-l-identite-de-l-etat/couleurs-palette
