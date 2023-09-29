7bb9fdc19f9ada12cbb9ed1c3f12cf509c8bead0
Cédric Soulas <soulascedric@gmail.com>
2023-09-29 19:04:55 +0200

    Utilise des tuiles pour le choix d'une région

--- src/routes/index/region-card.svelte
+++ src/routes/index/region-card.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import vizImg from "@gouvfr/dsfr/dist/artwork/pictograms/digital/data-visualization.svg";
+
   export let region: string;
 </script>
 
 <div class="fr-col-md-3 fr-col-12">
-  <div class="fr-card">
-    <div class="fr-card__body">
-      <div class="fr-card__content">
-        <h3 class="fr-card__title">
+  <div
+    class="fr-tile fr-tile--sm fr-tile--horizontal fr-enlarge-link"
+    id="tile-6661"
+  >
+    <div class="fr-tile__body">
+      <div class="fr-tile__content">
+        <h3 class="fr-tile__title">
           <a href="/regions/{region}/">{region}</a>
         </h3>
+      </div>
+    </div>
+    <div class="fr-tile__header">
+      <div class="fr-tile__pictogram">
+        <img src={vizImg} alt="" />
       </div>
     </div>
   </div>
