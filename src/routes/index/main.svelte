69c9d76a14d960925bcbffd0ee95dbdc461f5ffc
Cédric Soulas <soulascedric@gmail.com>
2023-09-29 19:23:47 +0200

    Ajoute un titre et un bouton vers a propos sur la page d'accueil

--- src/routes/index/main.svelte
+++ src/routes/index/main.svelte
@@ -5,25 +5,34 @@
   const regions = getRegionsNames();
 </script>
 
-<p class="max-w-2xl">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum. <a href="/a-propos"
-    >À propos</a
-  >
-</p>
+<h1 class="mt-12 max-w-xl">
+  Visualiser les objectifs nationaux de baisse des GES
+</h1>
+<div class="mb-16">
+  <p class="max-w-2xl">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <p>
+    <a class="fr-btn" href="/a-propos"
+      >En savoir plus sur le simulateur territorial</a
+    >
+  </p>
+</div>
 
-<h1 class="mb-2 mt-4">Régions</h1>
-
-<p class="max-w-2xl">
-  Sélectionnez votre région pour visualiser à titre indicatif la part de votre
-  territoire dans les objectifs nationaux de baisse des GES.
-</p>
-<div class="fr-grid-row fr-grid-row--gutters fr-mb-1w w-full">
-  {#each regions as region}
-    <RegionCard {region} />
-  {/each}
+<h2 class="mb-2 mt-4">Choisir une région</h2>
+<div class="mb-16">
+  <p class="max-w-2xl">
+    Sélectionnez votre région pour visualiser à titre indicatif la part de votre
+    territoire dans les objectifs nationaux de baisse des GES.
+  </p>
+  <div class="fr-grid-row fr-grid-row--gutters fr-mb-1w w-full">
+    {#each regions as region}
+      <RegionCard {region} />
+    {/each}
+  </div>
 </div>
