a8365bafa399010b2dc70e0090c242e4e8346f32
Cédric Soulas <soulascedric@gmail.com>
2023-09-30 21:13:11 +0200

    Regroupe visuellement les élements d'aide à la navigations

--- src/routes/regions/[slug]/navigation-bar.svelte
+++ src/routes/regions/[slug]/navigation-bar.svelte
@@ -32,36 +32,38 @@
   </div>
 </nav>
 
-<ul
-  class="fr-btns-group fr-btns-group--sm fr-btns-group--inline-sm fr-btns-group--icon-left"
->
-  <li>
-    <a
-      class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-arrow-left-line fr-btn--icon-left"
-      href={backUrl}>{backLabel}</a
-    >
-  </li>
-  {#if nextLabel}
+<div class="mb-8 border-t border-gray-200 px-8 pb-3 pt-5 shadow-md">
+  <div class="fr-stepper" style={nextLabel ? "" : "margin-bottom:64px"}>
+    <h2 class="fr-stepper__title">
+      <span class="fr-stepper__state">Étape {step} sur 4</span>
+      {title}
+    </h2>
+    <div
+      class="fr-stepper__steps"
+      data-fr-current-step={step}
+      data-fr-steps="4"
+    ></div>
+    <p class="fr-stepper__details">
+      <span class="fr-text--bold">Étape suivante : </span>{nextLabel}
+    </p>
+  </div>
+
+  <ul
+    class="fr-btns-group fr-btns-group--sm fr-btns-group--inline-sm fr-btns-group--icon-left"
+  >
     <li>
       <a
-        class="fr-btn fr-btn--tertiary fr-icon-arrow-right-line fr-btn--icon-right"
-        href={nextUrl}>{nextLabel}</a
+        class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-arrow-left-line fr-btn--icon-left"
+        href={backUrl}>{backLabel}</a
       >
     </li>
-  {/if}
-</ul>
-
-<div class="fr-stepper">
-  <h2 class="fr-stepper__title">
-    <span class="fr-stepper__state">Étape {step} sur 4</span>
-    {title}
-  </h2>
-  <div
-    class="fr-stepper__steps"
-    data-fr-current-step={step}
-    data-fr-steps="4"
-  ></div>
-  <p class="fr-stepper__details">
-    <span class="fr-text--bold">Étape suivante : </span>{nextLabel}
-  </p>
+    {#if nextLabel}
+      <li>
+        <a
+          class="fr-btn fr-btn--tertiary fr-icon-arrow-right-line fr-btn--icon-right"
+          href={nextUrl}>{nextLabel}</a
+        >
+      </li>
+    {/if}
+  </ul>
 </div>
