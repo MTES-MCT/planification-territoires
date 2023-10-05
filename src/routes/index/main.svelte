c169f7d1161fa6606f475e6a7c3b2cc190da410f
Cédric Soulas <soulascedric@gmail.com>
2023-10-05 15:48:05 +0200

    Remplace le bouton d'accès à A propos par un simple lien

--- src/routes/index/main.svelte
+++ src/routes/index/main.svelte
@@ -16,9 +16,7 @@
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-  <p>
-    <a class="fr-btn" href="/a-propos"
+    <a class="fr-link" href="/a-propos"
       >En savoir plus sur le simulateur territorial</a
     >
   </p>
