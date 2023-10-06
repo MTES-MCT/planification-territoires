3c3a61d6859e1aaa5e12f8ee515a427e138087a5
Cédric Soulas <soulascedric@gmail.com>
2023-10-06 21:54:38 +0200

    Improve text inside treemap with hyphens, ellipsis and variable sizes

--- src/lib/treemap/mondrian.svelte
+++ src/lib/treemap/mondrian.svelte
@@ -36,8 +36,6 @@
         d3.descending(a.value, b.value)
       )
   );
-
-  const uid = `O-${Math.random().toString(16).slice(2)}`;
 </script>
 
 <div class="h-full">
@@ -69,30 +67,37 @@
         }
       </style>
       <DiagonalHatchPattern />
-      {#each root.leaves() as d, i}
+      {#each root.leaves() as d}
         {@const lines = getLabel(d.data).split(/\n/g)}
+        {@const width = d.x1 - d.x0}
+        {@const height = d.y1 - d.y0}
         <g transform="translate({d.x0},{d.y0})">
           <ProgressBlock
-            width={d.x1 - d.x0}
-            height={d.y1 - d.y0}
+            {width}
+            {height}
             fill={getColor(d.data.sector)}
             progress={getProgressionRatio(d.data)}
           />
           <title>{getTitle(d.data)}</title>
-          <clipPath id="{uid}-clip-{i}">
-            <rect width={d.x1 - d.x0} height={d.y1 - d.y0} />
-          </clipPath>
-          <text clip-path="url(#{uid}-clip-{i})" pointer-events="none">
-            {#each lines as l, j}
-              <tspan
-                x="6"
-                y="{1.2 + (j === lines.length - 1) * 1.2 + j * 1.6}em"
-                class="line-{j}"
-              >
-                {l}
-              </tspan>
-            {/each}
-          </text>
+          <foreignObject {width} {height}>
+            <div class="h-full pl-2 pr-1 pt-2 font-medium tracking-tighter">
+              {#if height > 60 && width > 80}
+                <div class="mb-[1px] hyphens-auto text-sm leading-[1.1]">
+                  {lines[0]}
+                </div>
+                <div class="truncate text-xs font-normal">{lines[1]}</div>
+              {:else if height > 40 && width > 40}
+                <div class="truncate text-[12px] leading-none">
+                  {lines[0]}
+                </div>
+                <div class="truncate text-xs font-normal">{lines[1]}</div>
+              {:else if height > 16 && width > 50}
+                <div class="-mt-[3px] truncate text-[10px] leading-tight">
+                  {lines[0]}
+                </div>
+              {/if}
+            </div>
+          </foreignObject>
         </g>
       {/each}
     </svg>
