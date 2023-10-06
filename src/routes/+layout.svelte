bc29a56afeffb0082a6f60654a0a750b390851b9
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-10-06 15:57:35 +0200

    Matomo tracker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
   import "@gouvfr/dsfr/dist/dsfr.module.min.js";
-  import "../app.postcss";
+  import { page } from "$app/stores";
   import "$lib/assets/dsfr.css";
+  import "../app.postcss";
 
   import Header from "./layout/header.svelte";
   import Footer from "./layout/footer.svelte";
+
+  import { browser } from "$app/environment";
+  function trackPageView() {
+    if (browser && window._paq) {
+      window._paq.push(["setCustomUrl", $page.url.pathname]);
+      window._paq.push(["setDocumentTitle", $page.data.title]);
+      window._paq.push(["trackPageView"]);
+    }
+  }
+  $: $page.url.pathname, trackPageView();
 </script>
 
 <Header />
