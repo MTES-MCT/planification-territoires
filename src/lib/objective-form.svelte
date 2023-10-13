7d494ac9210ca3ec7abe82b3f8a1183b61b2f513
Cédric Soulas <soulascedric@gmail.com>
2023-10-13 16:36:30 +0200

    Crée un composant ObjectifForm réutilisable pour la prochaine étape

---
+++ src/lib/objective-form.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  import { getColor } from "$lib/utils.js";
+  import DiagonalHatchPattern from "$lib/treemap/diagonalHatchPattern.svelte";
+  import ProgressBlock from "$lib/treemap/progressBlock.svelte";
+  import SubTitle from "$lib/sub-title.svelte";
+  import DataDescription from "$lib/data-description.svelte";
+  import type { Action } from "$lib/types";
+
+  export let action: Action;
+  export let progress: number | undefined = undefined;
+  export let valuePhys: number;
+  export let valueCO2: number;
+  export let handlePhysInputChanged: (e: Event) => void;
+  export let handleCO2InputChanged: (e: Event) => void;
+
+  export let inputLabel: string;
+</script>
+
+<div class="flex flex-col">
+  <svg width="100%" height={50}>
+    <style>
+      .title {
+        font-size: 18px;
+        font-weight: bold;
+        fill-opacity: 0.75;
+      }
+    </style>
+    <DiagonalHatchPattern />
+    <ProgressBlock height={200} fill={getColor(action.sector)} {progress} />
+    <title>{action.leverName}</title>
+    <text>
+      <tspan class="title" x="16" y="32">
+        {action.leverName}
+      </tspan>
+    </text>
+  </svg>
+
+  <h1 class="sr-only">{action.leverName}</h1>
+  <div
+    class="flex flex-col border border-t-0 p-4"
+    style={`border-color:${getColor(action.sector)}`}
+  >
+    <div class="flex gap-x-4">
+      <div class="flex-1 border-r pb-6">
+        <div class="max-w-[180px] pr-4">
+          <SubTitle label="Objectif à atteindre en 2030" />
+        </div>
+      </div>
+      <SubTitle label={inputLabel} />
+    </div>
+
+    <div class="flex gap-x-4">
+      <DataDescription value={action.objPhys} unit={action.unitPhys} />
+      <div class="flex-1">
+        <label class="sr-only" for={action.id}>
+          {action.unitPhys}
+        </label>
+        <input
+          class="fr-input"
+          name={action.id}
+          type="number"
+          step="any"
+          min={0}
+          id={action.id}
+          value={valuePhys}
+          on:input={handlePhysInputChanged}
+        />
+      </div>
+    </div>
+
+    <div class="flex gap-x-4">
+      <div
+        class="flex-1 border-r py-2 text-right text-xs font-medium text-gray-500"
+      >
+        <span class="pr-4">Soit</span>
+      </div>
+      <div class="flex-1 py-2 text-right text-xs font-medium text-gray-500">
+        Soit
+      </div>
+    </div>
+
+    <div class="flex gap-x-4">
+      <DataDescription
+        value={action.objPhys / action.ratioCO2toPhys}
+        unit={action.unitCO2}
+      />
+      <div class="flex-1">
+        <label class="sr-only" for={`${action.id}-co2`}>
+          {action.unitCO2}
+        </label>
+        <input
+          class="fr-input"
+          name={`${action.id}-co2`}
+          type="number"
+          step="any"
+          min={0}
+          id={`${action.id}-co2`}
+          value={valueCO2}
+          on:input={handleCO2InputChanged}
+        />
+      </div>
+    </div>
+  </div>
+</div>
--- src/lib/treemap/progressBlock.svelte
+++ src/lib/treemap/progressBlock.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   export let width: number | undefined = undefined;
   export let height: number;
-  export let progress: number;
+  export let progress: number | undefined = undefined;
   export let fill: string;
 </script>
 
 <rect {fill} width={width ?? "100%"} {height} />
-<rect
-  fill="url(#diagonalHatch)"
-  x={0}
-  width={width ? width * progress : `${progress * 100}%`}
-  {height}
-/>
+{#if progress !== undefined}
+  <rect
+    fill="url(#diagonalHatch)"
+    x={0}
+    width={width ? width * progress : `${progress * 100}%`}
+    {height}
+  />
+{/if}
