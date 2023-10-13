648fdce7429b887ba6d93997655e2df3fba5aea9
Cédric Soulas <soulascedric@gmail.com>
2023-10-13 14:55:41 +0200

    Ajoute un bouton pour imprimer ou enregistrer en PDF

--- src/routes/territoire/[slug]/resultats/+page.svelte
+++ src/routes/territoire/[slug]/resultats/+page.svelte
@@ -25,10 +25,15 @@
   <MainViz data={data.regionData} showProgression />
 
   <button
-    class="fr-btn fr-btn--secondary mb-12 mt-4 print:!hidden"
+    class="fr-btn fr-btn--secondary mt-4 print:!hidden"
     on:click={() => {
       navigator.clipboard.writeText($page.url.href);
     }}
     >Copier le lien de partage
+  </button>
+  <button
+    class="fr-btn fr-btn--secondary mb-12 mt-2 print:!hidden"
+    on:click={() => print()}
+    >Imprimer ou enregistrer en PDF
   </button>
 </NavigationBar>
