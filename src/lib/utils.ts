5ccae9a6b76dc1941b747c7be62bc7aed96ba78f
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-10-21 19:05:34 +0200

    Corrige les problèmes d'arrondi sur les ambitions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,8 @@ import { arrange, distinct, filter, select, tidy } from "@tidyjs/tidy";
 
 const actionsData = rawActionsData.map((row) => ({
   ...row,
-  objPhys: row.objCO2 * row.ratioCO2toPhys,
+  objCO2: Math.round(row.objCO2),
+  objPhys: Math.round(row.objCO2 * row.ratioCO2toPhys),
   regionSlug: normalizeString(row.region),
 })) as Action[];
 
