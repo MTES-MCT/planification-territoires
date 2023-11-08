2365647d0faff752dc932d260fa487274ffbf671
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-11-08 17:18:54 +0100

    Ajout des mentions légales

--- src/routes/mentions-legales/+page.svelte
+++ src/routes/mentions-legales/+page.svelte
@@ -9,6 +9,28 @@
   </li>
 </Breadcrumbs>
 
-<div id="contenu" class="max-w-3xl">
+<div id="contenu" class="mb-16 max-w-3xl">
   <h1>Mentions légales</h1>
+
+  <h2>Éditeur de la plateforme</h2>
+
+  <p>La plateforme « Simulateur territorial » est éditée par :</p>
+  <p>Le Secrétariat général de la planification écologique,  situé :</p>
+  <p>
+    19 rue de Constantine <br />75007 Paris<br /> France
+  </p>
+
+  <h2>Directeur de la publication</h2>
+
+  <p>
+    Le Directeur de la publication est Monsieur Antoine Pellion, Secrétaire
+    général à la planification écologique.
+  </p>
+
+  <h2>Hébergement de la plateforme</h2>
+
+  <p>Cette plateforme est hébergée par :</p>
+  <p>
+    Scalingo <br />13 rue Jacques Peirotes<br /> 67000 Strasbourg <br />France
+  </p>
 </div>
