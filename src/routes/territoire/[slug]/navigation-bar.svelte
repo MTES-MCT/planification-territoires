ee62cb7ffe711c45e8508dfcffb63f1328e0a4e0
Cédric Soulas <soulascedric@gmail.com>
2024-01-15 18:37:20 +0100

    Affichage simplifié quand il n'y a qu'une seule étape

--- src/routes/territoire/[slug]/navigation-bar.svelte
+++ src/routes/territoire/[slug]/navigation-bar.svelte
@@ -27,39 +27,43 @@
 </h1>
 
 <div class="border-t border-gray-200 py-4 print:hidden" id="navigation">
-  <div class="fr-stepper" style={nextLabel ? "" : "margin-bottom:64px"}>
-    <h2 class="fr-stepper__title">
-      <span class="fr-stepper__state">Étape {step} sur {totalSteps}</span>
-      {territoryName} : {title}
-    </h2>
-    <div
-      class="fr-stepper__steps"
-      data-fr-current-step={step}
-      data-fr-steps={totalSteps}
-    ></div>
-    <p class="fr-stepper__details">
-      <span class="fr-text--bold">Étape suivante : </span>{nextLabel}
-    </p>
-  </div>
+  {#if totalSteps > 1}
+    <div class="fr-stepper" style={nextLabel ? "" : "margin-bottom:64px"}>
+      <h2 class="fr-stepper__title">
+        <span class="fr-stepper__state">Étape {step} sur {totalSteps}</span>
+        {territoryName} : {title}
+      </h2>
+      <div
+        class="fr-stepper__steps"
+        data-fr-current-step={step}
+        data-fr-steps={totalSteps}
+      ></div>
+      <p class="fr-stepper__details">
+        <span class="fr-text--bold">Étape suivante : </span>{nextLabel}
+      </p>
+    </div>
 
-  <ul
-    class="fr-btns-group fr-btns-group--sm fr-btns-group--inline-sm fr-btns-group--icon-left flex justify-end"
-  >
-    <li>
-      <a
-        class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-arrow-left-line fr-btn--icon-left"
-        href={backUrl}>Étape précédente</a
-      >
-    </li>
-    {#if nextLabel}
+    <ul
+      class="fr-btns-group fr-btns-group--sm fr-btns-group--inline-sm fr-btns-group--icon-left flex justify-end"
+    >
       <li>
         <a
-          class="fr-btn fr-btn--tertiary fr-icon-arrow-right-line fr-btn--icon-right"
-          href={nextUrl}>Étape suivante</a
+          class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-arrow-left-line fr-btn--icon-left"
+          href={backUrl}>Étape précédente</a
         >
       </li>
-    {/if}
-  </ul>
+      {#if nextLabel}
+        <li>
+          <a
+            class="fr-btn fr-btn--tertiary fr-icon-arrow-right-line fr-btn--icon-right"
+            href={nextUrl}>Étape suivante</a
+          >
+        </li>
+      {/if}
+    </ul>
+  {:else}
+    <h2 class="fr-stepper__title mb-0 mr-4">{territoryName} : {title}</h2>
+  {/if}
 </div>
 <div id="contenu">
   <slot name="no-sticky" />
